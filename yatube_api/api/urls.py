79b856fa151d7fa1f2198c18cd6edef3bf58c824
zz <asdfgtrewq2485@mail.ru>
2026-01-25 20:06:32 +0700

    fsd

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -7,7 +7,7 @@ from .views import GroupViewSet, PostViewSet, CommentViewSet
 router = DefaultRouter()
 router.register('api/v1/groups', GroupViewSet)
 router.register('api/v1/posts', PostViewSet)
-router.register(r'api/v1/posts/\d/comments', CommentViewSet)
+router.register(r'api/v1/posts/\d+/comments', CommentViewSet)
 
 urlpatterns = [
     path('api-token-auth/', views.obtain_auth_token),
