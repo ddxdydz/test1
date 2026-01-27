e07eb5a8e5f98c53c100172ab0933e8022ccf272
zz <asdfgtrewq2485@mail.ru>
2026-01-27 11:22:05 +0700

    dfs

--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -13,6 +13,9 @@ class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
 
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
     def perform_update(self, serializer):
         if serializer.instance.author != self.request.user:
             raise PermissionDenied('Изменение чужого контента запрещено!')
